b40814c84366f43ea283805a39593851b28dd1b1
machinefixer <waywardson.wayne@gmail.com>
2014-10-28 10:44:19 +0800

    functional-js update

---
+++ functional-js/Filter.js
@@ -0,0 +1,9 @@
+function getShortMessages(messages) {
+	return messages.filter(function(x) {
+		return x.message.length < 50;
+	}).map(function(x) {
+		return x.message;
+	})
+}
+
+module.exports = getShortMessages;
